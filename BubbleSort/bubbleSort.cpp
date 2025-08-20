f7d451295298d41a3c633a8561c0e84c6adddaad
sajalprajapati <prajapatisajal00@gmail.com>
2025-08-20 22:09:19 +0530

    BubbleSort

---
+++ BubbleSort/bubbleSort.cpp
@@ -0,0 +1,18 @@
+#include<iostream>
+using namespace std;
+int main(){
+    int n=6;
+    int arr[6]={10,1,7,n,14,9};
+
+    //we are implementing the bubble sort....
+    for(int i=1;i<n;i++) {//<------in bubble sort the number of rounds we perform is n-1.....
+       
+        for(int j=0;j<n-i;j++){ //<---- in bubble sort ,we compare adjacent elements in this loop....
+          if(arr[i]>arr[i+1]){
+            swap(arr[i],arr[i+1]);
+          }
+        }
+    }
+
+    
+}

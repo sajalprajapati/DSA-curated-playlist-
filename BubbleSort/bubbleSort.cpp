f22f67fb5e0a22589326bfd03bb3a9922c4c0efd
sajalprajapati <prajapatisajal00@gmail.com>
2025-08-21 21:20:43 +0530

    Bubble Sort Understanding done

--- BubbleSort/bubbleSort.cpp
+++ BubbleSort/bubbleSort.cpp
@@ -6,11 +6,17 @@ int main(){
 
     //we are implementing the bubble sort....
     for(int i=1;i<n;i++) {//<------in bubble sort the number of rounds we perform is n-1.....
-       
+       bool swapped=false;
         for(int j=0;j<n-i;j++){ //<---- in bubble sort ,we compare adjacent elements in this loop....
           if(arr[i]>arr[i+1]){
             swap(arr[i],arr[i+1]);
+            swapped=true;
           }
+        }
+
+
+        if(swapped==false){
+          break;
         }
     }
 
